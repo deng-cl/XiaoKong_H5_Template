6d8007ea36f4b43c2c32dd8299254ffe5b73bf0d
kongxh <2180733045@qq.com>
2025-07-21 15:03:26 +0800

    fix(http): 更新 HRequest  Axios 二次封装代码

--- src/utils/http/baseImple/index.ts
+++ src/utils/http/baseImple/index.ts
@@ -1,75 +1,5 @@
 import axios from 'axios'
-import type { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosRequestConfig } from 'axios'
-
-class HRequest {
-    instance: AxiosInstance
-    interceptors: Partial<HRequestInterceptors> | undefined /* Partial: 统一转换可选类型 */
-
-    constructor(config: /* HRequestConfig: 对原来的 axios 中的 AxiosRequestConfig 接口进行扩展*/ HRequestConfig) {
-        this.instance =
-            axios.create(
-                config,
-            ) /* 每次 new HRquest 时，都是一个独立的 axios 实例供下面所要封装的请求方法使用 <业务需要可能要创建多个 instance 实例: 如当项目中需要使用多个请求 baseURL 时> */
-
-        /* 实例拦截器 <对当前实例所有方法都会生效>: 调用自定义拦截器 Hooks : 创建 HRequest 实例或使用下面实例封装的方法时可自定义配置拦截器 hooks 来做对应的拦截操作 */
-        this.interceptors = config.interceptors /* 获取 config 中拦截器参数 hooks */
-        this.instance.interceptors.request.use(
-            this.interceptors?.requestInterceptor || ((config) => config),
-            this.interceptors?.requestInterceptorCatch || ((error) => Promise.reject(error)),
-        )
-
-        this.instance.interceptors.response.use(
-            this.interceptors?.responseInterceptor || ((response) => response),
-            this.interceptors?.responseInterceptorCatch || ((error) => Promise.reject(error)),
-        )
-    }
-
-    request<R>(config: HRequestConfig<R>): Promise<R> {
-        if (config?.interceptors?.requestInterceptor) {
-            /* 局部请求拦截器 */
-            config = config.interceptors.requestInterceptor(
-                config as InternalAxiosRequestConfig /* 通过类型断言进行类型范围延申，使其符合对应的拦截器类型 */,
-            )
-        }
-
-        return this.instance
-            .request<any, R>(config)
-            .then((res) => {
-                /* 通过实例中的 instance/axios 实例，来进行对应请求的封装 */
-                if (config?.interceptors?.responseInterceptor) {
-                    /* 局部响应拦截器 */
-                    res = config.interceptors.responseInterceptor(res) /* 将最终的 res 断言成 R 类型 */
-                }
-                return res
-            })
-            .catch((err) => {
-                if (config?.interceptors?.responseInterceptorCatch) {
-                    err = config.interceptors.responseInterceptorCatch(err)
-                }
-                return err
-            })
-    }
-
-    get<R>(url: string, config: Omit<HRequestConfig<R>, 'url'> = {}): Promise<R> {
-        return this.request<R>({ ...config, url, method: 'GET' })
-    }
-
-    post<R>(url: string, config: Omit<HRequestConfig<R>, 'url'> = {}): Promise<R> {
-        return this.request<R>({ ...config, url, method: 'POST' })
-    }
-
-    delete<R>(url: string, config: Omit<HRequestConfig<R>, 'url'> = {}): Promise<R> {
-        return this.request<R>({ ...config, url, method: 'DELETE' })
-    }
-
-    patch<R>(url: string, config: Omit<HRequestConfig<R>, 'url'> = {}): Promise<R> {
-        return this.request<R>({ ...config, url, method: 'PATCH' })
-    }
-
-    put<R>(url: string, config: Omit<HRequestConfig<R>, 'url'> = {}): Promise<R> {
-        return this.request<R>({ ...config, url, method: 'PUT' })
-    }
-}
+import type { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosRequestConfig, Method } from 'axios'
 
 /** HRequest 实例 config 配置对象类型声明 */
 export interface HRequestConfig<T = HInternalAxiosResponseConfig> extends AxiosRequestConfig {
@@ -78,7 +8,6 @@ export interface HRequestConfig<T = HInternalAxiosResponseConfig> extends AxiosR
 
 /** 拦截器扩展类型声明 */
 export interface HRequestInterceptors<T = HInternalAxiosResponseConfig> {
-    /* 定义 HRequestInterceptors 接口: 用于扩展 axios 中的 config 的 AxiosRequestConfig，扩展 axios 拦截器参数 */
     requestInterceptor: (config: HInternalAxiosRequestConfig) => HInternalAxiosRequestConfig
     requestInterceptorCatch: (error: any) => any
     responseInterceptor: (config: T) => T
@@ -92,6 +21,57 @@ export interface HInternalAxiosRequestConfig extends InternalAxiosRequestConfig 
 /** 响应拦截器类型扩展 */
 export interface HInternalAxiosResponseConfig extends AxiosResponse {
     [key: string]: any
+}
+
+// --------------------------------------------- HRequest 实现
+class HRequest {
+    instance: AxiosInstance
+    interceptors: Partial<HRequestInterceptors> | undefined
+
+    constructor(config: HRequestConfig) {
+        // -- 创建 axios 实例
+        this.instance = axios.create(config)
+
+        // -- 全局拦截器解构
+        const { requestInterceptor, requestInterceptorCatch, responseInterceptor, responseInterceptorCatch } = (this.interceptors = config.interceptors) ?? {}
+
+        // -- 全局请求拦截器
+        this.instance.interceptors.request.use(requestInterceptor || ((config) => config), requestInterceptorCatch || ((error) => Promise.reject(error)))
+
+        // -- 全局响应拦截器
+        this.instance.interceptors.response.use(responseInterceptor || ((response) => response), responseInterceptorCatch || ((error) => Promise.reject(error)))
+    }
+
+    // -- 公共调用方法
+    async request<R>(config: HRequestConfig<R>): Promise<R> {
+        // -- 局部拦截器解构
+        const { requestInterceptor, responseInterceptor, responseInterceptorCatch } = config.interceptors ?? {}
+
+        // -- 局部请求拦截器
+        if (requestInterceptor) config = requestInterceptor(config as InternalAxiosRequestConfig)
+
+        // -- 发起请求
+        try {
+            const result = await this.instance.request<any, R>(config)
+            return responseInterceptor ? responseInterceptor(result) : result // -- 局部响应拦截器
+        } catch (error: any) {
+            return responseInterceptorCatch ? responseInterceptorCatch(error) : error // -- 局部响应拦截器
+        }
+    }
+
+    // -- 动态生成 HTTP 方法(闭包)
+    private createHttpMethod(method: Method) {
+        return <R>(url: string, config: Omit<HRequestConfig<R>, 'url'> = {}) => {
+            return this.request<R>({ ...config, url, method })
+        }
+    }
+
+    // -- 快捷请求方法
+    public get = this.createHttpMethod('GET')
+    public post = this.createHttpMethod('POST')
+    public delete = this.createHttpMethod('DELETE')
+    public patch = this.createHttpMethod('PATCH')
+    public put = this.createHttpMethod('PUT')
 }
 
 export * from './utils'
--- src/utils/http/baseImple/utils.ts
+++ src/utils/http/baseImple/utils.ts
@@ -1,30 +1,35 @@
-import type HRequest from "."
-import type { HRequestConfig } from "."
+import HRequest from '.'
+import type { HRequestConfig } from '.'
 
-type RequestMethodType = 'GET' | 'POST' | 'DELETE' | 'PATCH' | 'PUT'
+// -- 请求方法类型
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
 
-interface HFetchList {
-    key: string
-    url: string
-    method: RequestMethodType
+// -- 请求 config 配置类型
+type HttpConfig = Omit<HRequestConfig, 'url' | 'method' | 'data' | 'params'>
+
+// -- 生成的请求函数 → 通过泛型 R 来提供请求响应结果的类型声明
+type FetchGenFunction<R = any> = (data?: any, config?: HttpConfig) => Promise<R>
+type GoodGenerateFetchFunctions<T> = {
+    [K in keyof T]: FetchGenFunction<T[K]>
 }
 
-type HFetchListTuple = [string, RequestMethodType, string]
-
-type FetchGenFunctionType = (
-    data?: any,
-    config?: Omit<HRequestConfig, 'url' | 'method' | 'data' | 'params'>,
-) => Promise<any>
-
+// -- 传入的参数列表类型 → 通过泛型 K 来限制传入的 key 类型
+interface FetchList<K> {
+    key: K
+    url: string
+    method: HttpMethod
+}
+type FetchListTuple<K> = [key: K, url: string, HttpMethod]
+type FetchListInput<K> = (FetchList<K> | FetchListTuple<K>)[]
 
 /**
- * 批量生成请求方法工具函数
- * @param http
- * @param fetchList
- * @useExample const { fetchExample1, fetchExample2 } = fetchFunctionBatchGenerator(http, [{ key:'fetchExample1', method: 'GET', url: '...', ... }, { key:'fetchExample2', method: 'GET', url: '...', ... }])
- * @returns FetchGenFunctionType[]
+ * 通过配置的形式 - 快速批量生成请求函数
+ * @genericity 泛型 T extends Record<string, any> → 传入可用于限制 fetchList 配置,与提供更好的提示
+ * @param http 传入 HRequest 请求实例
+ * @param fetchList 请求函数生成配置列表
+ * @returns GoodGenerateFetchFunctions<T> → 返回所生成的请求函数对象
  */
-export const fetchFunctionBatchGenerator = (http: HRequest, fetchList: (HFetchList | HFetchListTuple)[]) => {
+export const fetchFunctionBatchGenerator = <T extends Record<string, any> = Record<string, any>>(http: HRequest, fetchList: FetchListInput<keyof T>): GoodGenerateFetchFunctions<T> => {
     const fetchFunctions = {}
 
     fetchList.forEach((fetchItem) => {
@@ -32,19 +37,33 @@ export const fetchFunctionBatchGenerator = (http: HRequest, fetchList: (HFetchLi
         patchGenFetch(key, url, method)
     })
 
-    return fetchFunctions as Record<string, FetchGenFunctionType>
+    return fetchFunctions as GoodGenerateFetchFunctions<T>
 
+    // -- 生成请求函数方法
     function patchGenFetch(key, url, method) {
-        fetchFunctions[key] = async (
-            data?: any,
-            config: Omit<HRequestConfig, 'url' | 'method' | 'data' | 'params'> = {},
-        ) => {
+        fetchFunctions[key] = async (data?: any, config: HttpConfig = {}) => {
             const fetchConfig = { url, method, ...config } as HRequestConfig
 
-            // -- 请求参数处理
             fetchConfig[method == 'GET' ? 'params' : 'data'] = data
 
             return http.request(fetchConfig)
         }
     }
 }
+
+/** Using Example: fetchFunctionBatchGenerator
+ *  - example 1
+        const http = new HRequest({})
+        const { getUsername, getUserAge } = fetchFunctionBatchGenerator<{ getUsername: string; getUserAge: number }>(http, [
+            { key: 'getUsername', url: '/user/name', method: 'GET' },
+            ['getUserAge', '/user/age', 'GET']
+        ])
+  
+ *  - example 2 → curried
+        const http = new HRequest({})
+        export const fetchFunctionBatchGeneratorCurry = curry(fetchFunctionBatchGenerator)(http)
+        const { getUsername, getUserAge } = fetchFunctionBatchGeneratorCurry<{ getUsername: string; getUserAge: number }>([
+            { key: 'getUsername', url: '/user/name', method: 'GET' },
+            ['getUserAge', '/user/age', 'GET']
+        ])
+ */
